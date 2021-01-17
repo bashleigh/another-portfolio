ff21e5eff6c2b0ab33f00c8839896b3ec0eb2539
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-01-17 21:29:57 +0000

    feat: added terminal actions

--- src/components/terminal.tsx
+++ src/components/terminal.tsx
@@ -1,8 +1,46 @@
 import React, { useState } from "react";
 import "./terminal.scss";
 
+const commands: { [s: string]: (input: string[]) => string[] } = {
+  hello: (input: string[]) => {
+    if (input[0] && input[0] === "--help") {
+      return [
+        "Hello command",
+        "returns a greeting",
+        "hello [name]",
+        "",
+        'hello Bob: returns "Hello Bob',
+      ];
+    }
+    return [`Hello! ${input[0] || ""}`];
+  },
+  // commands: (input: string[]) => [
+  //   'Commands available:',
+  //   '',
+  //   'hello',
+  //   'commands',
+  //   'about',
+  //   'contact',
+  // ],
+};
+
+const runCommand = (input: string): string[] => {
+  if (input === "") {
+    return [""];
+  }
+
+  const inputs = input.split(" ");
+  if (!inputs[0] || !Object.keys(commands).includes(inputs[0])) {
+    return [`command: "${inputs[0]}" not found`];
+  }
+
+  return commands[inputs.shift()](inputs);
+};
+
 export const Terminal = () => {
   const [input, setInput] = useState<string>("");
+  const [output, setOutput] = useState<string[]>([]);
+  const [commandExists, setCommandExists] = useState<boolean>(false);
 
   return (
     <div id="terminal">
@@ -14,7 +52,9 @@ export const Terminal = () => {
         </div>
       </header>
       <div className="terminal-body">
-        <p className="has-text-weight-bold">Ashleigh Simonelli</p>
+        <p className="has-text-weight-bold">Hello! I'm Ashleigh</p>
+        <br />
+        <p>Here's some key points that I'm good at.</p>
         <ul>
           <li>✅ Web Developer</li>
           <li>✅ App Developer</li>
@@ -23,26 +63,51 @@ export const Terminal = () => {
           <li>✅ EPOS Builder</li>
           <li>✅ Football Fanatic (viva Biancazzurri!) </li>
         </ul>
+        <br />
+        <p className="text-info">
+          Type "hello" into the terminal window and hit enter
+        </p>
+        {output.map((txt, index) => (
+          <p key={`${txt}-${index}`}>{txt}</p>
+        ))}
+        <br />
         <div className="terminal-control">
-          <p className="terminal-input">
+          <form
+            className="terminal-input"
+            onSubmit={event => {
+              event.preventDefault();
+              const result = runCommand(input);
+              setOutput([...output, ...result]);
+              setInput("");
+            }}
+            onClick={() => {
+              // TODO focus on input
+            }}
+          >
             <div className="field">
               <label className="label">~</label>
 
               <div className="control has-icons-left">
                 <span className="icon is-left">❯</span>
                 <input
-                  className="input"
+                  className={`input ${commandExists ? " command-exists" : ""}`}
                   name=""
-                  onChange={event => setInput(event.target.value)}
+                  autoFocus={true}
+                  onChange={event => {
+                    const input = event.target.value;
+                    const command = input.split(" ").shift();
+                    setCommandExists(Object.keys(commands).includes(command));
+                    setInput(input);
+                  }}
                   value={input}
                 />
                 <span
                   className="cursor"
-                  style={{ left: `${input.length * 9.5 + 10}px` }}
+                  style={{ left: `${input.length * 10 + 15}px` }}
                 ></span>
               </div>
             </div>
-          </p>
+          </form>
         </div>
       </div>
       <nav className="terminal-tmux-bar">
