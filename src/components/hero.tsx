ba617e0e4d15ce4ad95778b1800d7b5b78a39593
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-01-17 22:10:06 +0000

    feat: added phone component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Phone } from "./phone";
 import { Terminal } from "./terminal";
 
 export const Hero = () => (
   <div className="hero is-fullheight">
     <div className="hero-body">
       <div className="container">
-        <div className="columns is-centered">
-          <div className="column is-6">
+        <div className="columns is-centered is-vcentered">
+          <div className="column is-narrow">
+            <Phone />
+          </div>
+          <div className="column is-narrow">
             <Terminal />
           </div>
         </div>
---
+++ src/components/phone.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Ashleigh from "../images/ashleigh.svg";
+import "./phone.scss";
+
+export const Phone = () => (
+  <div id="phone">
+    <div className="inner-skin">
+      <div className="phone-content">
+        <h1 className="title">Hello There, Stranger!</h1>
+        <Ashleigh />
+        <h2 className="subtitle">Let's build your dream!</h2>
+      </div>
+    </div>
+  </div>
+);
