493695729c889525d7f478df6af2ee616129050a
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-01-21 16:09:13 +0000

    feat: basic 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const NotFound = () => (
+  <div className="hero is-primary">
+    <h6 className="title">404</h6>
+    <h6 className="subtitle">Looks like that page wasn't found!</h6>
+  </div>
+);
