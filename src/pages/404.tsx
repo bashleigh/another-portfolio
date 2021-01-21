b3c892b885a02307729ef3af90a732948f6db7d6
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-01-21 16:59:29 +0000

    fix: default export 40

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const NotFound = () => (
+export default () => (
   <div className="hero is-primary">
     <h6 className="title">404</h6>
     <h6 className="subtitle">Looks like that page wasn't found!</h6>
