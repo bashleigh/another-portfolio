4fa26a4bd39b166292242956fb03c414995e5ca8
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2021-09-01 11:54:42 +0100

    feat: updated recruitment status

--- src/components/recruitment.tsx
+++ src/components/recruitment.tsx
@@ -6,6 +6,6 @@ export const RecruitmentNotifier = () => (
   <div className="recruitment-notifier">
     <div className="title is-6">Recruitment Status</div>
     <span className="notification-status"></span>
-    Might be Interested
+    Not Looking
   </div>
 );
