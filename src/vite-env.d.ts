7168f13a52825b4e6b7b5c01f705630258324edc
ashleigh <ashleighsimonelli@gmail.com>
2025-11-16 09:23:55 +0000

    feat: added google analytics setup

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,13 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_GA_TRACKING_ID?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module "*.webp?url" {
   const value: string
   export default value
@@ -9,4 +17,3 @@ declare module "*.webp" {
   const value: string
   export default value
 }
-
